7222055e6165b04ab55bf6a4ad9480efcddd8460
MikeWencel <wencel.michael@gmail.com>
2020-01-06 17:28:50 +0100

    All about string

---
+++ C#/Section_One/String_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mike";
+            string lastName = "Wencel";
+
+            Console.WriteLine(name + " " + lastName);
+
+            string capsMessage = lastName.ToUpper();
+
+            Console.WriteLine(name + " " + capsMessage);
+
+            string lowerCase = name.ToLower();
+
+            Console.WriteLine(lowerCase + " " + capsMessage);
+        }
+    }
+}
