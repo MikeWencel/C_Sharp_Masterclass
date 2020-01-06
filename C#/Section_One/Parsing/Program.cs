27aa17a04d4da4b3f12923268b7f9acf666a979d
MikeWencel <wencel.michael@gmail.com>
2020-01-06 17:44:38 +0100

    Done

---
+++ C#/Section_One/Parsing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecString = "13";
+            string result = myString + mySecString;
+            int numOne = Int32.Parse(myString);
+            int numTwo = Int32.Parse (mySecString);
+            int resultInt = numOne + numTwo;
+            Console.WriteLine(result);
+            Console.WriteLine(resultInt);
+        }
+    }
+}
