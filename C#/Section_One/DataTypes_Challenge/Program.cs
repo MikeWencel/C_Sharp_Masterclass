cc28746d57d031e808038e82200e974819a740c1
MikeWencel <wencel.michael@gmail.com>
2020-01-07 22:11:56 +0100

    Datatypes

---
+++ C#/Section_One/DataTypes_Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataTypes_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 23344;
+            byte myByte = 24;
+            sbyte mySbyte = 126;
+            uint myuINt = 4323232;
+            short myShort = 32;
+            ushort myUshort = 6544;
+            long myLong = 312312312;
+            ulong myUlung = 321312;
+            float myFloat = 312312F;
+            Char something = 'D';
+            bool isTrue = true;
+            string myString = "test string";
+
+
+
+
+        }
+    }
+}
