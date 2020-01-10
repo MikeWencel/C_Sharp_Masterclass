d5dd81df8543948d771d4d5791970184fed3e1ed
MikeWencel <wencel.michael@gmail.com>
2020-01-10 21:56:54 +0100

    Const

---
+++ C#/Section_One/ConstantsC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConstantsC
+{
+    class Program
+    {
+
+        // constants as field
+        // We cannot changed const variable
+
+        const double PI = 3.14;
+        const int weeks = 52, month = 12;
+        const string birthday = "20.09.1987";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My birthday is always going to be: {0}",birthday);
+        }
+    }
+}
