36d136781b67fce7da2ed28a4e85108bc8e98503
MikeWencel <wencel.michael@gmail.com>
2020-02-09 00:11:20 +0100

    user_input

---
+++ C#/Section_Two/User_input/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace User_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
