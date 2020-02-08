030d07a0641b62fb06a41d1295d7d17c9a6a1ec6
MikeWencel <wencel.michael@gmail.com>
2020-02-08 23:58:44 +0100

    return_methods

--- C#/Section_Two/return_methods/Program.cs
+++ C#/Section_Two/return_methods/Program.cs
@@ -7,11 +7,16 @@ namespace return_methods
         static void Main(string[] args)
         {
             Console.WriteLine(Add(2,5));
+            Console.WriteLine(Multiply(2,5));
         }
 
         public static int Add(int first, int second){
 
             return first + second;
+        }
+
+        public static int Multiply(int first, int second){
+            return first * second;
         }
     }
 }
