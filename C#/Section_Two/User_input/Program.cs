a88905e7ba8b91bc291b819fd47038a4c20bafb9
MikeWencel <wencel.michael@gmail.com>
2020-02-09 12:49:49 +0100

    Calculator with inputs and switch statements

--- C#/Section_Two/User_input/Program.cs
+++ C#/Section_Two/User_input/Program.cs
@@ -6,8 +6,33 @@ namespace User_input
     {
         static void Main(string[] args)
         {   
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
+            Console.WriteLine(Calculator());
+        }
+
+        public static double Calculator(){
+            Console.WriteLine("Please enter the number:");
+            string firstInput = Console.ReadLine();
+            Console.WriteLine("Please enter second number");
+            String secondInput = Console.ReadLine();
+            int first = Int32.Parse(firstInput);
+            int second = Int32.Parse(secondInput);
+            Console.WriteLine("What do you want to do ? Please type number");
+            Console.WriteLine("add, odd, multiply, divided");
+            string answer = Console.ReadLine();
+
+            switch(answer){
+                case "add":
+                return first + second;
+                case "odd":
+                return first - second;
+                case "multiply":
+                return first * second;
+                case "divided":
+                return first / second;
+                default:
+                return -1;
+            }
+        
         }
     }
 }
