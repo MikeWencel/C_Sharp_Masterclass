c34f3b04085d69570e25b3301f6d150776edac3b
MikeWencel <wencel.michael@gmail.com>
2020-02-10 18:56:44 +0100

    Try_catch done

---
+++ C#/Section_Two/Try_catch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string userInput = Console.ReadLine();
+            try {
+                int userInputNumber = int.Parse(userInput);
+            }
+            catch (FormatException){
+                Console.WriteLine("Format exception, please enter the correct type next time.");
+            }
+            catch(OverflowException){
+                Console.WriteLine("The number was too long or too short for an Int32");
+            }
+            finally
+            {
+                Console.WriteLine("It works anyway");
+            }
+            
+
+        }
+    }
+}
