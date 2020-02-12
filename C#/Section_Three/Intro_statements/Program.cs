5b277d4a11c12ac93404e420561f651c1aed61e9
MikeWencel <wencel.michael@gmail.com>
2020-02-12 17:40:12 +0100

    statements

---
+++ C#/Section_Three/Intro_statements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Intro_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temperature = Console.ReadLine();
+            int temp;
+            int number;
+            bool numberEntered = int.TryParse(temperature, out number);
+
+            if(numberEntered){
+                temp = number;
+            }
+            else
+            {
+                temp = 0;
+                Console.WriteLine("Temperature is set to {0}, you should type a number!", temp);
+            }
+
+            if(temp <= 15){
+                Console.WriteLine("You need to take coat!!");
+            }else if (temp > 15 && temp <= 21){
+                Console.WriteLine("You don't need coat!!");
+            }else
+            {
+                Console.WriteLine("Take the sunglasses!");
+            }
+          
+        }
+    }
+}
