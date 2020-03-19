e5ebc2549087db1c360afd061ed78105e4f512de
MikeWencel <wencel.michael@gmail.com>
2020-03-19 14:20:14 +0100

    switch

---
+++ C#/Section_Three/switch_statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace switch_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            System.Console.WriteLine("How old are you?");
+            string ageStr = Console.ReadLine();
+
+            int age = Int32.Parse(ageStr);
+
+            switch (age)
+            {
+                case 15:
+                Console.WriteLine("You're too young!");
+                break;
+                case 25:
+                Console.WriteLine("Your good to go!");
+                break;
+                default:
+                Console.WriteLine("so how old are you?");
+                break;
+            }
+        }
+    }
+}
