88982e2f68f9dc04002841302defad2daa913ef9
MikeWencel <wencel.michael@gmail.com>
2020-04-08 23:44:19 +0200

    Loops review

---
+++ C#/Section_Three/Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   String hole = " ";
+            for (int i = 0; i < 5; i++)
+            
+            {
+                System.Console.WriteLine("*");
+                for(int j = 0; j < 5;j++){
+                    if(i == 0){
+                        System.Console.WriteLine("*");
+                    }else if((i == 1 || i == 5) & j == 1){
+
+                        System.Console.WriteLine("*");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
