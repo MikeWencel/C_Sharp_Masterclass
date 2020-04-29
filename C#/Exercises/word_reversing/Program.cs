7b2e0ed2c958f62a8ffbd4fcb7ada5fed915c03f
MikeWencel <wencel.michael@gmail.com>
2020-04-29 17:48:22 +0200

    reversing word

---
+++ C#/Exercises/word_reversing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+
+namespace word_reversing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(Solution("Kasiulka"));
+            Console.WriteLine(Alternative("Kasiulka"));
+            
+            
+        }
+
+        static string Solution(string word){
+        char[] charArr = word.ToCharArray(0,word.Length);      
+        Array.Reverse(charArr);
+        string newStr = new string(charArr);
+        return newStr;
+
+        }
+
+        //Alternative
+        static string Alternative(string str){
+            return new string(str.ToArray().Reverse().ToArray());
+    
+        }
+
+    }
+}
+
+       
+    
+
