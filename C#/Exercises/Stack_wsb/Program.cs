ae7645f8ef504324fc5e84b6b9984bb5c181d8af
MikeWencel <wencel.michael@gmail.com>
2020-11-13 10:53:29 +0100

    You're doing greatgit add .

---
+++ C#/Exercises/Stack_wsb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Stack_wsb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> letters = new Stack<string>();
+
+            String x = Console.ReadLine();
+            if(x == "+"){
+                letters.Push(Console.ReadLine());
+            }else {
+                
+            }
+
+            foreach(string el in letters){
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
+
+        }
+    }
+}
