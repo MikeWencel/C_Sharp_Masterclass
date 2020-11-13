3ee97b9fe6b6bf8edc9c2956144e62f768f3a60b
unknown <MikeWencel>
2020-11-13 11:11:05 +0100

    You're doing great!

---
+++ C#/Exercises/comparing_big_letters_wsb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comparing_big_letters_wsb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
