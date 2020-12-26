12e24771de6c203fd1d358f5af39d4a9882e4a04
MikeWencel <wencel.michael@gmail.com>
2020-12-26 12:19:59 +0100

    You're doing great!

--- C#/Section_One/Datatypes_Overiview/Datatypes_Overiview/Program.cs
+++ C#/Section_One/Datatypes_Overiview/Datatypes_Overiview/Program.cs
@@ -14,7 +14,33 @@ namespace Datatypes_Overiview
             float e = 99.99f;
             double f = 1.5;
             decimal g = 2.3232312321M;
+            bool h = true;
+            char singleLetter = 'A';
+            string username = "Jason";
             //Smaller date, that you wnato to store - use smaller datatypes
+
+            int first_num = 2;
+            int second_num = 4;
+            first_num++;
+
+            int sum;
+            sum = first_num + second_num;
+
+            double third_num = 2.5;
+            double fourth_num = 1.6;
+
+            double second_sum = third_num / fourth_num;
+
+            Console.WriteLine(second_num);
+
+            sum = second_num / first_num;
+
+      
+
+
+
+            Console.WriteLine(sum);
+            Console.Read();
 
 
             
