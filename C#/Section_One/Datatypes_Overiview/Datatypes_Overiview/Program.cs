3ab774508fb17ae2de7552fb9c3b5733be8d141a
MikeWencel <wencel.michael@gmail.com>
2020-12-26 12:09:12 +0100

    You're doing great!

---
+++ C#/Section_One/Datatypes_Overiview/Datatypes_Overiview/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Datatypes_Overiview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 120;
+            short b = 30000;
+            int c = 200000000;
+            long d = 20000000000000;
+            //It's important to add f as last letter! It will read as double!
+            float e = 99.99f;
+            double f = 1.5;
+            decimal g = 2.3232312321M;
+            //Smaller date, that you wnato to store - use smaller datatypes
+
+
+            
+            
+
+
+        }
+    }
+}
