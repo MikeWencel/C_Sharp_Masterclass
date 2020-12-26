80ea19f7b36b717fd05857181a70142d37f88764
MikeWencel <wencel.michael@gmail.com>
2020-12-26 13:40:35 +0100

    You're doing great!

--- C#/Section_One/Console_Methods/Console_Methods/Program.cs
+++ C#/Section_One/Console_Methods/Console_Methods/Program.cs
@@ -6,20 +6,17 @@ namespace Console_Methods
     {
         static void Main(string[] args)
         {
-            //Print in the same line
-            Console.Write("Print in the same line");
-            //Print in the next line, cursos in the next line
-            Console.WriteLine("Prints and puts cursor in the next line!");
-            //Return ASCII value, it's take single input
+            
             Console.Write("Enter a string and press ENTER: ");
             string input = Console.ReadLine();
             Console.WriteLine("You have entered {0}", input);
 
-            Console.Write("Enter ASCII value: ");
-            int asciiValue = Console.Read();
-            Console.WriteLine("Ascii value is {0}", asciiValue);
-            Console.ReadKey();
-            
+            Console.Write("Enter letter: ");
+            int valueAscii = Console.Read();
+            Console.WriteLine("Ascii value is {0}", valueAscii);
+
+            Console.ReadLine();
+
 
         }
     }
