1ddd73bfa0968ab4517b853d382ec97e8decac39
MikeWencel <wencel.michael@gmail.com>
2020-12-31 13:09:09 +0100

    You're doing great!

---
+++ C#/Section_One/StringChallenge/StringChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            Console.WriteLine("Enter your name");
+            input = Console.ReadLine();
+
+            Console.WriteLine(input.ToUpper());
+            Console.WriteLine(input.ToLower());
+            Console.WriteLine(input.Trim());
+            Console.WriteLine(input.Substring(0,3));
+
+            Console.Read();
+
+
+        }
+    }
+}
