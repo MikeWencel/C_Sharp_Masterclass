645c9fe3c54d66d3fdd12ce94c77f1ff34a1cf46
MikeWencel <wencel.michael@gmail.com>
2021-01-01 23:41:41 +0100

    You're doing great!

---
+++ C#/Section_One/StringChallengeTwo/StringChallengeTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringChallengeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String here:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Enter index to search: ");
+            string indexInput = Console.ReadLine();
+            Console.WriteLine(input.IndexOf(indexInput));
+
+            Console.WriteLine("Enter first name");
+            string firstName = Console.ReadLine();
+            firstName.Trim();
+            Console.WriteLine("Enter last name");
+            string lastName = Console.ReadLine();
+            lastName.Trim();
+
+            Console.WriteLine("{0} {1}", firstName, lastName);
+
+        }
+    }
+}
