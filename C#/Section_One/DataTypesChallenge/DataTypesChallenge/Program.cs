7b36ac53facc452f23bb14df8c7f734b18da5d87
MikeWencel <wencel.michael@gmail.com>
2021-01-02 17:59:12 +0100

    You're doing great!

---
+++ C#/Section_One/DataTypesChallenge/DataTypesChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTypesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            String name = "Mike";
+            int num = 23243;
+            double numero = 2.4;
+            float number = 2.4F;
+            long numeros = 2321312421432L;
+
+            String controller = "I control text";
+
+         
+
+        }
+    }
+}
