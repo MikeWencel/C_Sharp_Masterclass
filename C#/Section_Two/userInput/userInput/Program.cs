6f8ff09af8c3a67c815bff09a220514bbb26141e
MikeWencel <wencel.michael@gmail.com>
2021-01-05 20:57:32 +0100

    You're doing great!

---
+++ C#/Section_Two/userInput/userInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace userInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate());
+            Console.Read();
+
+        }
+
+        public static double Calculate()
+        {
+            Console.WriteLine("Please write your first number!");
+            string first = Console.ReadLine();
+            double numOne = Double.Parse(first);
+            Console.WriteLine("Please write your second number!");
+            string second = Console.ReadLine();
+            double numTwo = Double.Parse(second);
+
+            return numOne + numTwo;
+        }
+        
+    }
+}
