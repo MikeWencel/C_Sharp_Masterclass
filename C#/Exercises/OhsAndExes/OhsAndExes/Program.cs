dd914047182ad3564e77ce2f4420fe11a0a8187a
MikeWencel <wencel.michael@gmail.com>
2021-01-05 20:26:27 +0100

    Done

---
+++ C#/Exercises/OhsAndExes/OhsAndExes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OhsAndExes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#/Exercises/SPIN_PL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SPIN_PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = Console.ReadLine();
+            int num = Int32.Parse(word);
+            int count = 0;
+
+            while(count < num){
+                String tempWord = Console.ReadLine();
+                String [] tempTable = tempWord.Split(",");
+                // Console.WriteLine(tempTable[1]);
+
+                String [] tempNumberOne = tempTable[0].Split("=");
+                String [] tempNumberTwo = tempTable[1].Split("=");
+
+                int n = Int32.Parse(tempNumberOne[1]);
+                int m = Int32.Parse(tempNumberTwo[1]);
+                
+
+                count++;
+            }
+        }
+    }
+}
