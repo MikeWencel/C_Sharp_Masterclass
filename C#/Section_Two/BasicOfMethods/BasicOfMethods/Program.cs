dfac3aa5fc930bdae9c1677da57a667729b02548
MikeWencel <wencel.michael@gmail.com>
2021-01-05 20:35:02 +0100

    You're doing great!

---
+++ C#/Section_Two/BasicOfMethods/BasicOfMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasicOfMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+        
+            Console.WriteLine();
+
+            WriteSomethingPara("Mike");
+
+            Console.Read();
+
+        }
+
+        public static void WriteSomething()
+        {
+            string word = Console.ReadLine();
+        }
+
+        public static void WriteSomethingPara(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
