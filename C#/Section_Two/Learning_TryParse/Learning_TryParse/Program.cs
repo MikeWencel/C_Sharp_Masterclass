f24b7bcdda642d2b26658d31ff3653a34588358a
MikeWencel <wencel.michael@gmail.com>
2021-02-07 11:38:14 +0100

    You're doing great!

---
+++ C#/Section_Two/Learning_TryParse/Learning_TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Learning_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "123";
+            int parsedValue;
+            bool success = int.TryParse(number, out parsedValue);
+
+            if(success)
+            {
+                Console.WriteLine("Parsing value successful = number is: " + parsedValue);
+            }
+
+            else
+            {
+                Console.WriteLine("Parsing fail");
+            }
+
+            Console.Read();
+        }
+    }
+}
